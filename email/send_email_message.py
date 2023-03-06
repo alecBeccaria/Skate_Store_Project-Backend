4b387380fc6800639c71840112c9113d2df20341
alecBeccaria <abecc2255@gmail.com>
2023-03-05 21:51:34 -0700

    Fixed  email body

--- email/send_email_message.py
+++ email/send_email_message.py
@@ -6,11 +6,14 @@ queue_url = "https://sqs.us-east-1.amazonaws.com/249804032116/Skate_Queue"
 
 
 def lambda_handler(event, context):
-    email = event['email']
+    sent_to = event["email"]["sent_to"]
+    body = event["email"]["body"]
 
-    # send sqs message with the current date & time
+    formattedEmail = {"sent_to": sent_to, "body": body}
+
     message = client.send_message(
         QueueUrl=queue_url,
-        MessageBody=json.dumps(email),
+        MessageBody=json.dumps(formattedEmail),
     )
+
     return {"statusCode": 200, "body": json.dumps(message, indent=2)}
