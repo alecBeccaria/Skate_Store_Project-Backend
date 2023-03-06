5eda03b1b7c957a0cad6078c1163fb8aea6f61c2
alecBeccaria <abecc2255@gmail.com>
2023-03-05 23:13:09 -0700

    User_Get checks for both admin or user

--- user/User_Get.py
+++ user/User_Get.py
@@ -1,5 +1,6 @@
 import boto3
 from os import getenv
+import json
 
 region_name = getenv("APP_REGION")
 
@@ -15,14 +16,27 @@ def read(event):
     username = event["params"]["path"]["username"]
 
     dynamodb = boto3.resource("dynamodb", region_name=region_name)
-    admins_table = dynamodb.Table("Users")
+    admins_table = dynamodb.Table("Admins")
+    users_table = dynamodb.Table("Users")
 
     key = {"username": username}
 
-    Item = admins_table.get_item(Key=key)
-    admin = Item["Item"]
-    admin.pop('password')
-    return {
-        'statusCode': 200,
-        'body': admin
-    }
+    try:
+        Item = admins_table.get_item(Key=key)
+        print(Item)
+        if Item["Item"] != None:
+            print("admin found!")
+            return {"statusCode": 200, "body": Item["Item"]}
+    except Exception as e:
+        print(e)
+        try:
+            Item = users_table.get_item(Key=key)
+            if Item["Item"] != None:
+                print("user found!")
+                return {"statusCode": 200, "body": Item["Item"]}
+
+        except Exception as e:
+            print(e)
+            return {"statusCode": 400, "body": "User not found in db"}
+
+    
